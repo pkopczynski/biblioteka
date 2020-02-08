a55e30ec28a3c81579b6c1efa99e391077d47fe4
Your Name <you@example.com>
2020-02-08 11:21:03 +0100

    commit try

--- src/Manage/components/BookItem.tsx
+++ src/Manage/components/BookItem.tsx
@@ -9,8 +9,8 @@ interface IBookItemProps {
 
 export const BookItem = styled.div`
   background-image: url(${(props: IBookItemProps) => props.cover});
-  width: 200px;
   height: 300px;
+  width: 200px;
   background-size: cover;
   background-repeat: round;
   margin: 15px;
